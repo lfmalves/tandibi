d18a081b538610dcf73eb37ce867e67a0ef4ba9f
lfmalves <luis.projensino@gmail.com>
2021-12-31 08:35:23 -0300

    :requesting and :blocking scopes for Bond

--- app/models/bond.rb
+++ app/models/bond.rb
@@ -37,4 +37,6 @@ class Bond < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
   scope :following, -> { where(state: FOLLOWING) }
+  scope :requesting, -> { where(state: REQUESTING) }
+  scope :blocking, -> { where(state: BLOCKING) }
 end
